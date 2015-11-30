7743e98fc979e32a0b6426414798498034071dee
Sebastian Wimmer <sebastian.wimmer@outlook.com>
2015-11-30 14:05:19 +0100

    a

---
+++ TS/qt_example.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_PI">
+<context>
+    <name>Page</name>
+    <message>
+        <source>Translations Exarrrrmple</source>
+        <translation>Beispiel für Übersetzungen</translation>
+    </message>
+    <message>
+        <source>Arrrr %1</source>
+        <translation>Hallo %1</translation>
+    </message>
+</context>
+<context>
+    <name>installscript</name>
+    <message>
+        <location filename="../views/public.ui" line="36"/>
+        <source>This is a dynamically created page, arrrr</source>
+        <translation>Diese Seite wurde dynamisch erzeugt.</translation>
+    </message>
+</context>
+</TS>
